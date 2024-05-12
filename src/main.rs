4c8285c50483d3d4567f73b67310d634d0aec4b4
matteoo.eth <mateogalic112@gmail.com>
2024-05-12 11:08:20 +0200

    Add match statement

--- src/main.rs
+++ src/main.rs
@@ -8,32 +8,31 @@ struct Player {
     club: String,
 }
 
-fn main() -> Result<(), Error> {
+fn main() {
     const FILE_NAME: &str = "players.txt";
 
     let players = read_lines(FILE_NAME).unwrap();
 
+    println!("Please input player name: ");
     loop {
-        println!("Please input player name.");
-
         let mut player_name_guess: String = String::new();
 
         io::stdin()
             .read_line(&mut player_name_guess)
             .expect("Failed to read line");
 
-        if players
+        let found_player = players
             .iter()
-            .any(|player| player.name.to_lowercase() == player_name_guess.to_lowercase().trim())
-        {
-            println!("Player found!");
-            break;
-        } else {
-            println!("Player not found!");
+            .find(|player| player.name.to_lowercase() == player_name_guess.to_lowercase().trim());
+
+        match found_player {
+            Some(player) => {
+                println!("Player found: {:?}", player);
+                break;
+            }
+            None => println!("Player not found, try again: "),
         }
     }
-
-    Ok(())
 }
 
 fn parse_player_line(line: &str) -> Option<Player> {
