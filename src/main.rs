536a8a2eb4a000b9794d9ae1010f780b726304b8
matteoo.eth <mateogalic112@gmail.com>
2024-08-03 17:21:25 +0200

    Add player mod

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::read_to_string;
 use std::io::{self, Error};
 
-use crate::player::{Player, Position};
+use crate::player::Player;
 pub mod player;
 
 // TODO Extract modules
@@ -18,16 +18,17 @@ fn main() {
             .read_line(&mut player_name_guess)
             .expect("Failed to read line");
 
-        match find_player_by_name(&players, player_name_guess) {
+        match Player::find_player_by_name(&players, player_name_guess) {
             Some(player) => {
-                if is_oldest(&players, player) {
+                // Transfer plater to Chelsea
+                if Player::is_oldest(&players, player) {
                     println!(
                         "{} is the oldest player in the squad - {} years old.",
                         player.name, player.age
                     );
                 }
 
-                if is_most_valued(&players, player) {
+                if Player::is_most_valued(&players, player) {
                     println!(
                         "{} is the most valued player in the squad - {} million.",
                         player.name, player.market_value
@@ -48,71 +49,11 @@ fn main() {
     }
 }
 
-fn find_player_by_name(players: &Vec<Player>, player_name_guess: String) -> Option<&Player> {
-    players
-        .iter()
-        .find(|player| player.name.to_lowercase() == player_name_guess.to_lowercase().trim())
-}
-
-fn is_oldest(players: &Vec<Player>, player: &Player) -> bool {
-    players
-        .iter()
-        .filter(|other| other.name != player.name)
-        .all(|other| player.is_older(other))
-}
-
-fn is_most_valued(players: &Vec<Player>, player: &Player) -> bool {
-    players
-        .iter()
-        .filter(|other| other.name != player.name)
-        .all(|other| player.market_value > other.market_value)
-}
-
-fn parse_player_line(line: &str) -> Option<Player> {
-    let parts: Vec<&str> = line.split(" - ").collect();
-
-    if parts.len() != 5 {
-        return None;
-    }
-
-    let name = parts[0].trim().to_string();
-
-    let age = parts[1].trim().parse::<u8>();
-    let age = match age {
-        Ok(value) => value,
-        Err(_) => 0,
-    };
-
-    let position: Position = match parts[2].trim() {
-        "GK" => Position::Goalkeeper,
-        "CB" | "DL" | "DR" => Position::Defender,
-        "CM" | "DM" => Position::Midfielder,
-        "CF" | "ST" => Position::Forward,
-        _ => return None,
-    };
-
-    let club = parts[3].trim().to_string();
-
-    let market_value = parts[4].trim().parse::<u8>();
-    let market_value: u8 = match market_value {
-        Ok(value) => value,
-        Err(_) => 0,
-    };
-
-    Some(Player {
-        name,
-        age,
-        position,
-        club,
-        market_value,
-    })
-}
-
 fn read_lines(filename: &str) -> Result<Vec<Player>, Error> {
     let players: Vec<Player> = read_to_string(filename)
         .unwrap()
         .lines()
-        .filter_map(parse_player_line)
+        .filter_map(Player::create_from_line)
         .collect();
 
     Ok(players)
--- src/player/mod.rs
+++ src/player/mod.rs
@@ -2,8 +2,8 @@
 pub struct Player {
     pub name: String,
     pub age: u8,
-    pub position: Position,
-    pub club: String,
+    position: Position,
+    club: String,
     pub market_value: u8,
 }
 
@@ -27,8 +27,74 @@ impl Player {
         self.age > other.age
     }
 
+    pub fn is_more_valued(&self, other: &Player) -> bool {
+        self.market_value > other.market_value
+    }
+
     pub fn transfer(&mut self, new_club: String, fee: u8) {
         self.club = new_club;
         self.market_value = (self.market_value + fee) / 2;
+    }
+}
+
+impl Player {
+    pub fn find_player_by_name(players: &Vec<Player>, input_name: String) -> Option<&Player> {
+        players
+            .iter()
+            .find(|player| player.name.to_lowercase() == input_name.to_lowercase().trim())
+    }
+
+    pub fn is_oldest(players: &Vec<Player>, player: &Player) -> bool {
+        players
+            .iter()
+            .all(|other| other.name == player.name || player.is_older(other))
+    }
+
+    pub fn is_most_valued(players: &Vec<Player>, player: &Player) -> bool {
+        players
+            .iter()
+            .all(|other| other.name == player.name || player.is_more_valued(other))
+    }
+}
+
+impl Player {
+    pub fn create_from_line(line: &str) -> Option<Self> {
+        let parts: Vec<&str> = line.split(" - ").collect();
+
+        if parts.len() != 5 {
+            return None;
+        }
+
+        let name = parts[0].trim().to_string();
+
+        let age = parts[1].trim().parse::<u8>();
+        let age = match age {
+            Ok(value) => value,
+            Err(_) => 0,
+        };
+
+        let position: Position = match parts[2].trim() {
+            "GK" => Position::Goalkeeper,
+            "CB" | "DL" | "DR" => Position::Defender,
+            "CM" | "DM" => Position::Midfielder,
+            "CF" | "ST" => Position::Forward,
+            _ => return None,
+        };
+
+        let club = parts[3].trim().to_string();
+
+        let market_value = parts[4].trim().parse::<u8>();
+        let market_value: u8 = match market_value {
+            Ok(value) => value,
+            Err(_) => 0,
+        };
+
+        Some(Player {
+            name,
+            age,
+            position,
+            club,
+            market_value,
+        })
     }
 }
